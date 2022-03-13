a5dabf85c1d029422899c918c4de96308e56d10f
Sandy <sandy.mera.guerrero@utelvt.edu.ec>
2022-03-13 07:26:59 -0500

    actualizacion

---
+++ SandyGuerrero/suma.cpp
@@ -0,0 +1,12 @@
+#include<iostrema>
+using namespace std;
+int main ()
+{    
+	float A,B,C ;
+	cout<<"ingrese los 2 numeros A B : ;
+	cin>>A>>B;
+	c=A+B;
+	cout<<"el resultado es : ";
+	cout<<C;
+	return 0;
+}

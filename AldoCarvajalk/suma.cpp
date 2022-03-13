b6aa6c1217992726045904291a9c03e54d0186cb
Aldo Carvajal <aldo.carvajal.klinger@utelvt.edu.ec>
2022-03-13 16:46:08 -0500

    Ultima actualizacion realizada en mi pc

--- AldoCarvajalk/suma.cpp
+++ AldoCarvajalk/suma.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 int main(){
 	float A,B,C;
+	cout<<"Ingrese Los dos numeros A B: ";
 	cin>>A>>B;
 	C=A+B;
-	cout<<C;
+	cout<<"El resultado es el siguente: ";
+	cout<<C<<endl;
 	return 0;
 }
 

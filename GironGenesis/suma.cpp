dfc8501ae616a74bf02c8f730e022788c8e150fe
Genesis <genesis.giron.lupu@utelvt.edu.ec>
2022-03-12 21:02:42 -0500

    Primera actualización realizada desde mi celular

--- GironGenesis/suma.cpp
+++ GironGenesis/suma.cpp
@@ -1,10 +1,12 @@
-#include<iostrema>
-using namespace std;
+#include<iostrem>
+using namespace std; 
 int main()
 {
 float a,b,c;
+cout<<"Ingrese los dos números a b :";
 cin>>a>>b;
 c=a+b;
-cout<<c;
+cout<<"El resultado es :";
+cout<<c<<endl;
 return 0;
 }

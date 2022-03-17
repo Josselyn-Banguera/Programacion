a5426eed990d68fc406d419e255a046a1fb0ef87
Guerron-Sergio <fabianguerron1999@gmail.com>
2022-03-17 18:52:12 -0500

    Segunda actualizacion Guerron-Sergio

--- Guerron-Sergio/suma.cpp
+++ Guerron-Sergio/suma.cpp
@@ -2,8 +2,11 @@
 using namespace std:
 int main(){
 	float a,b,c:
+	cout<<"Ingrese los numeros a b: ":
 	cin>>a>>b:
 	c=a+b:
 	cout<<c:
+	cout<<"El resultado es este: ": 
+	cout<<c<<endl:
 	return 0:
 }

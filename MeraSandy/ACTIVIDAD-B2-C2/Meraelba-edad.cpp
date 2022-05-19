26848b5555a66479b885bfe4684028b4c5527be8
MeraElba <sandy.mera.guerreo@utelvt.edu.ec>
2022-05-19 17:19:34 -0500

    Actualizacion de la actividad -B2-C2

--- MeraSandy/ACTIVIDAD-B2-C2/Meraelba-edad.cpp
+++ MeraSandy/ACTIVIDAD-B2-C2/Meraelba-edad.cpp
@@ -1,29 +1,40 @@
-#include<iostream>
+//=======Nombre del programa: La edad
+//====Archivo: Merelba-edad.cpp
+//====Autor: Mera Sandy
+//====Fecha de elaboracion: 02-05-2022
+//====Fecha de ultima actualizacion: 19-05-2022
+//include<iostream>
 using namespace std;
 int main()
 {
-	int da,ma,aa,dn,mn,an,a,m,d;
-	cout<<"Ingrese la fecha actual: Dia Mes Año :";
-	cin>>da>>ma>>aa;
+	int MS_da,MS_ma,MS_aa,MS_dn,MS_mn,MS_an,MS_a,MS_m,MS_d;
+	cout<<"Ingrese la fecha actual: Dia Mes y Año :";
+	cin>>MS_da>>MS_ma>>MS_aa;
 	cout<<"Ingrese su fecha de nacimiento: Dia Mes Año :";
-	cin>>dn>>mn>>an;
-	if(da<dn){
-		da=da+30;
-		ma=ma-1;
-		d=da-dn;
+	cin>>MS_dn>>MS_mn>>MS_an;
+	if(MS_da<MS_dn){
+		MS_da=MS_da+30;
+		MS_ma=MS_ma-1;
+		MS_d=MS_da-MS_dn;
 	}
 	else{
-		d=da-dn;
+		MS_d=MS_da-MS_dn;
 	}
-	if(ma<mn){
-		ma=ma+12;
-		aa=aa-1;
-		m=ma-mn;
+	if(MS_ma<MS_mn){
+		MS_ma=MS_ma+12;
+		MS_aa=MS_aa-1;
+		MS_m=MS_ma-MS_mn;
 	}
 	else{
-		m=ma-mn;
+		MS_m=MS_ma-MS_mn;
 	}
-	a=aa-an;
-        cout<<"Usted tiene: " <<a<< "años" <<m<< "mese" <<d<< "y" <<d<< "dias"<<endl;
+	MS_a=MS_aa-MS_an;
+
+	cout<<"//=======Nombre de programa: Compara de dos numeros"<<endl;
+cout<<"//====Archivo: Meraelba-edad.cpp"<<endl;
+cout<<"//====Autor: Mera Sandy "<<endl;
+cout<<"//====Fecha de elaboracion: 02-05-2022"<<endl;
+cout<<"//====Fecha de ultima actializacion: 19-05-2022"<<endl;
+        cout<<"Usted tiene: " <<MS_a<< "años" <<MS_m<< "meses" <<MS_d<< "y" <<MS_d<< "dias"<<endl;
 	return(0);
 }

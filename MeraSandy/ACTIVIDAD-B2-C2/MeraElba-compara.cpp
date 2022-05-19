1c0b7d06168029cb9e3f1a5fb4f85c255daa273b
MeraElba <sandy.mera.guerreo@utelvt.edu.ec>
2022-05-19 16:35:54 -0500

    Actualizado compara de la actividad B2-C2

--- MeraSandy/ACTIVIDAD-B2-C2/MeraElba-compara.cpp
+++ MeraSandy/ACTIVIDAD-B2-C2/MeraElba-compara.cpp
@@ -1,25 +1,37 @@
-#include<iostream>
+///=======Nombre del programa: Compara de dos numeros
+//====Archivo: MeraElba-Compara.cpp
+//====Autor:Mera Sandy
+//====Fecha de elaboracion: 02-05-2022
+//====Fecha de ultima actualizacion: 19-05-2022
+//include<iostream>
 using namespace std;
 int main()
  {
-	 float X,Y;
-	 cout<<"ingrese el valor de X: ";
-	 cin>>X;
-	 cout<<"ingrese el valor de Y: ";
-	 cin>>Y;
-	 if(X==Y){
- cout<<"el valor de X="<<X<<"es igual a Y="<<Y<<endl;
+	 float MS_x,Ms_Y;
+	 cout<<"ingrese el valor de MS_x: ";
+	 cin>>MS_X;
+	 cout<<"ingrese el valor de MS_Y: ";
+	 cin>>MS_Y;
+	 if(MS_x==MS_y){
+ cout<<"el valor de MS_X="<<MS_X<<"es igual a MS_Y="<<MS_Y<<endl;
 
      }else{
-     if (X<Y){
-cout<<"el valor de X="<<X<<"es menor a Y="<<Y<<endl;
+     if (MS_X<MS_Y){
+
+ cout<<"//=======Nombre de programa: Compara de dos numeros"<<endl;
+cout<<"//====Archivo: MeraElba-Compara.cpp"<<endl;
+cout<<"//====Autor: Mera Sandy"<<endl;
+cout<<"//====Fecha de elaboracion: 02-05-2022"<<endl;
+cout<<"//====Fecha de ultima actializacion: 19-05-2022"<<endl;
+cout<<"el valor de MS_X="<<MS_X<<"es menor a MS_Y="<<MS_Y<<endl;
       }else{
 	
 
-cout<<"el valor de Y="<<Y<<"es menora X= "<<X<<endl;
+cout<<"el valor de MS_Y="<<MS_Y<<"es menora MS_X= "<<MS_X<<endl;
        }
        }
 	 return 0;
 
 
    }
+   

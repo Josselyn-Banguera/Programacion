38dae4c2b2b437636eb390b5677bb9ae20bfc22f
MeraElba <sandy.mera.guerreo@utelvt.edu.ec>
2022-05-19 18:36:10 -0500

    Actualizado actividad-B2-C2

--- MeraSandy/ACTIVIDAD-B2-C2/MeraElba-cuentamoneda.cpp
+++ MeraSandy/ACTIVIDAD-B2-C2/MeraElba-cuentamoneda.cpp
@@ -1,31 +1,42 @@
- #include<iostream>
+ //=======Nombre del programa: Cuenta moneda
+//====Archivo:MeraElba-cuentamoneda.cpp
+//====Autor: Mera Sandy
+//====Fecha de elaboracion: 02-05-2022
+//====Fecha de ultima actualizacion: 19-05-2022
+//include<iostream>
 using namespace std;
 int main()
 {
-	int n, c=0, c1=0, c2=0;
-       float x,a=0, a1=0, a2=0;
+       int MS-n, MS_c=0,S_c1=0, MS_c2=0;
+       float MS_x,MS_a=0, MS_a1=0, MS_a2=0;
        cout<<"¿Cuantas monedas va a ingresar?: ";
-       cin>>n;
+       cin>>MS_n;
        do{
       cout<<"Ingrese el valor de la moneda (0.10, 0.25): ";
-      cin>>x;
-      c=c+1;
-      a=a+x;
-      if(x==0.10){
+      cin>>MS_x;
+      MS_c=MS_c+1;
+      MS_a=MS_a+MS_x;
+      if(MS_x==0.10){
 
-	      c1=c1+1;
-	      a1=a1+x;
+	      MS_c1=MS_c1+1;
+	      MS_a1=MS_a1+MS_x;
       }else{
-	      c2=c2+1;
-	      a2=a2+x;
+	      MS_c2=MS_c2+1;
+	      MS_a2=MS_a2+MS_x;
       }
-       }while(c<n);
+       }while(MS_c<MS_n);
        cout<<"El resultado es :"<<endl;
-       cout<<"Cantidad de monedas ingresadas :"<<c<<endl;
-       cout<<"Total del dinero contado :"<<a<<endl;
-       cout<<"Cantidad de monedas de 0.10ctv ingresadas :"<<c1<<endl;
-       cout<<"Total del dinero de monedas de 0.10ctv :"<<a1<<endl;
-       cout<<"Cantidad de monedas de 0.25ctv ingresadas :"<<c2<<endl;
-       cout<<"Total del dinero de monedas de 0.25ctv :"<<a2<<endl;
+       cout<<"Cantidad de monedas ingresadas :"<<MS_c<<endl;
+       cout<<"Total del dinero contado :"<<MS_a<<endl;
+       cout<<"Cantidad de monedas de 0.10ctv ingresadas :"<<MS_c1<<endl;
+       cout<<"Total del dinero de monedas de 0.10ctv :"<<MS_a1<<endl;
+       cout<<"Cantidad de monedas de 0.25ctv ingresadas :"<<MS_c2<<endl;
+       cout<<"Total del dinero de monedas de 0.25ctv :"<<MS_a2<<endl;
+
+    cout<<"//=======Nombre de programa: Cuenta moneda"<<endl;
+cout<<"//====Archivo: MeraElba-cuentamoneda.cpp"<<endl;
+cout<<"//====Autor: Mera Sandy "<<endl;
+cout<<"//====Fecha de elaboracion: 02-05-2022"<<endl;
+cout<<"//====Fecha de ultima actializacion: 19-05-2022"<<endl;
        return(0);
 }

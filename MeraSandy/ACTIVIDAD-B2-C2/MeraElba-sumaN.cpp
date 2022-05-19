fcd662478dbfb503025d8be7c97ba98f24db0b56
MeraElba <sandy.mera.guerreo@utelvt.edu.ec>
2022-05-19 18:02:49 -0500

    Actualizado la actividad-B2-C2

--- MeraSandy/ACTIVIDAD-B2-C2/MeraElba-sumaN.cpp
+++ MeraSandy/ACTIVIDAD-B2-C2/MeraElba-sumaN.cpp
@@ -1,20 +1,32 @@
-#include<iostream>
+//=======Nombre del programa: Suma de dos numeros
+//====Archivo: MeraElba-sumaN.cpp
+//====Autor: Mera Sandy
+//====Fecha de elaboracion: 02-05-2022
+//====Fecha de ultima actualizacion: 19-05-2022
+//include<iostream>
  using namespace std;
 
    int main ()
      {
-	     int c=0,n;
-	     float s=0,x;
+	     int MS_c=0,MS_n;
+	     float MS_s=0,MS_x;
 	 cout<<"ingrese los numeros a sumar : ";
-         cin>>n;
+         cin>>MS_n;
 	 do{
 		 cout<<"ingrese un numero : ";
-		 cin>>x;
-		 c=c+1;
-		 s=s+x;
+		 cin>>MS_x;
+		 MS_c=MS_c+1;
+		 MS_s=MS_s+MS_x;
 
 
-	  }while(c<n) ;
-	  cout<<"la suma es : "<<s<<endl;
+	  }while(MS_c<MS_n) ;
+
+   cout<<"//=======Nombre de programa:suma de dos numeros"<<endl;
+
+	cout<<"//====Archivo: MeraElba-sumaN.cpp"<<endl;
+   cout<<"//====Autor:Mera Sandy "<<endl;
+   cout<<"//====Fecha de elaboracion: 02-05-2022"<<endl;
+   cout<<"//====Fecha de ultima actializacion: 19-05-2022"<<endl;
+	  cout<<"la suma es : "<<MS_s<<endl;
 	 return(0);
  }

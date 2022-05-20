4296242d0e9a6702e540dc08fe1a4f7e4890169e
AldoCarvajalk <aldo.carvajal.klinger@utelvt.edu.ec>
2022-05-20 12:21:49 -0500

    Actualizacion de Compara B2-C2

--- AldoCarvajalk/ACTIVIDAD-B2-C2/AldoCarvajalk-Compara.cpp
+++ AldoCarvajalk/ACTIVIDAD-B2-C2/AldoCarvajalk-Compara.cpp
@@ -1,21 +1,33 @@
+//=======Nombre del programa:Compara de dos numeros
+//====Archivo: AldoCarvajalk-Compara.cpp
+//====Autor:Carvajal Aldo
+//====Fecha de elaboracion: 03-05-2022
+//====Fecha de ultima actualizacion: 19-05-2022
 #include<iostream>
 using namespace std;
 int main()
 {
-	float A,B;
-	cout<<"ingrese A:";
-	cin>>A;
-	cout<<"ingrese B:";
-	cin>>B;
-	if(A==B) {
-	cout<<"El valor de A= "<<A<<" es igual a B "<<B<<endl;
+	float CA_A,CA_B;
+	cout<<"ingrese CA_A:";
+	cin>>CA_A;
+	cout<<"ingrese CA_B:";
+	cin>>CA_B;
+	if(CA_A==CA_B) {
+	cout<<"El valor de CA_A= "<<CA_A<<" es igual a CA_B "<<CA_B<<endl;
 
 	}else{
-		if(A<B){
+		if(CA_A<CA_B){
 
-	cout<<"El valor de A=" <<A<<" es menor a B="<<B<<endl;
+cout<<"//=======Nombre de programa: Compara de dos numeros"<<endl;
+cout<<"//====Archivo: AldoCarvajalk-Compara.cpp"<<endl;
+cout<<"//====Autor: Carvajal Aldo"<<endl;
+cout<<"//====Fecha de elaboracion: 03-05-2022"<<endl;
+cout<<"//====Fecha de ultima actualizacion:19-05-2022"<<endl;
+
+
+	cout<<"El valor de CA_A=" <<CA_A<<" es menor a CA_B="<<CA_B<<endl;
 		}else{
-	cout<<"El valor de B="<<B<<" es menor a A="<<A<<endl;
+	cout<<"El valor de CA_B="<<CA_B<<" es menor a CA_A="<<CA_A<<endl;
 		}
 	}
 	return 0;

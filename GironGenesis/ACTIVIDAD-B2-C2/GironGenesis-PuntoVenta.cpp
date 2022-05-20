835dde8255b7c209c3f26aa68eeef7c1093b3ddb
Genesis Girón <genesis.giron.lupu@utelvt.edu.ec>
2022-05-19 23:52:04 -0500

    Actualización PuntoVenta B2-C2

--- GironGenesis/ACTIVIDAD-B2-C2/GironGenesis-PuntoVenta.cpp
+++ GironGenesis/ACTIVIDAD-B2-C2/GironGenesis-PuntoVenta.cpp
@@ -1,29 +1,37 @@
+//=======Nombre del programa: PUNTO VENTA  
+//====Archivo: GironGenesis-PuntoVenta.cpp
+//====Autor: Giron Genesis 
+//====Fecha de elaboración: 02-05-2022
+//====Fecha de última actualización: 19-05-2022
+
 #include<iostream>
 using namespace std;
 int main()
 {
-	int c=0, P;
-	float a=0,x,tb,viva,vdes,iva=0.12,des=0.30,vf;
+	int GG_c=0, GG_P;
+	float GG_a=0, GG_x, GG_tb, GG_viva, GG_vdes, GG_iva=0.12, GG_des=0.30, GG_vf;
 	cout<<"Ingrese la cantidad de productos: ";
-	cin>>P;
+	cin>>GG_P;
 	do{
-		cout<<"Ingrese el valor de los productos: ";
-		cin>>x;
-		c=c+1;
-		a=a+x;
-		viva=a*iva;
-		cout<<"El valor del iva de la compra es de: $"<<viva<<endl;
-		vdes=a*des;
-		cout<<"El valor del descuento de su compra es de:$"<<vdes<<endl;
-		tb=viva+vdes;
-	}while(c<P);
-	if(tb==a){
-		cout<<"Su compra aplica para un descuento: "<<tb<<endl;
-	iva=tb*viva/100;
-	des=tb*vdes/100;
+	cout<<"Ingrese el valor de los productos: ";
+		cin>>GG_x;
+		GG_c=GG_c+1;
+		GG_a=GG_a+GG_x;
+		GG_viva=GG_a*GG_iva;
+cout<<"El valor del iva de la compra es de: $"<<GG_viva<<endl;
+		GG_vdes=GG_a*GG_des;
+cout<<"El valor del descuento de su compra es de:$"<<GG_vdes<<endl;
+		GG_tb=GG_viva+GG_vdes;
+	}while(GG_c<GG_P);
+	if(GG_tb==GG_a){
+cout<<"Su compra aplica para un descuento: "<<GG_tb<<endl;
+	GG_iva=GG_tb*GG_viva/100;
+	GG_des=GG_tb*GG_vdes/100;
 }else{
-	vf=a+viva-vdes;
-	cout<<"El valor final es de:$ "<<vf<<endl;
+	GG_vf=GG_a+GG_viva-GG_vdes;
+
+cout<<"//=======Nombre del programa: PUNTO VENTA"<<endl;   cout<<"//====Archivo: GironGenesis-PuntoVenta.cpp "<<endl; cout<<"//====Autor: Giron Genesis "<<endl;                 cout<<"//====Fecha de elaboración: 02-05-2022 "<<endl;     cout<<"//====Fecha de última actualización: 19-05-2022 "<<endl;
+cout<<"El valor final es de:$ "<<GG_vf<<endl;
 }
 return 0;
 }

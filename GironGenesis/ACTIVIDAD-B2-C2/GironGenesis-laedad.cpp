756e185038dca15a71ce2abc067e5d9d98219fc3
Genesis Girón <genesis.giron.lupu@utelvt.edu.ec>
2022-05-20 00:52:43 -0500

    Actualización ladeada B2-C2

--- GironGenesis/ACTIVIDAD-B2-C2/GironGenesis-laedad.cpp
+++ GironGenesis/ACTIVIDAD-B2-C2/GironGenesis-laedad.cpp
@@ -1,29 +1,41 @@
+//=======Nombre del programa: Compara de dos números 
+//====Archivo: GironGenesis-Compara.cpp
+//====Autor: Giron Genesis 
+//====Fecha de elaboración: 02-05-2022
+//====Fecha de última actualización: 17-05-2022
 #include<iostream>
 using namespace std;
 int main()
 {
-	int da,ma,aa,dn,mn,an,a,m,d;
+int GG_da,GG_ma,GG_aa,GG_dn,GG_mn,GG_an,GG_a,GG_m,GG_d;
 	cout<<"Ingrese la fecha actual: Dia Mes Año :";
-	cin>>da>>ma>>aa;
-	cout<<"Ingrese su fecha de nacimiento: Dia Mes Año :";
-	cin>>dn>>mn>>an;
-	if(da>=dn){
-		d=da-dn;
+	cin>>GG_da>>GG_ma>>GG_aa;
+cout<<"Ingrese su fecha de nacimiento: Dia Mes Año :";
+	cin>>GG_dn>>GG_mn>>GG_an;
+	if(GG_da>=GG_dn){
+		GG_d=GG_da-GG_dn;
 	}
 	else{
-		da=da+30;
-                ma=ma-1;
-                d=da-dn;
+		GG_da=GG_da+30;
+                GG_ma=GG_ma-1;
+		GG_d=GG_da-GG_dn;
 	}
-	if(ma>=mn){
-		ma=ma+12;
-		aa=aa-1;
-		m=ma-mn;
+	if(GG_ma>=GG_mn){
+		GG_ma=GG_ma+12;
+		GG_aa= GG_aa-1;
+		GG_m=GG_ma-GG_mn;
 	}
 	else{
-		a=aa-an;
+		GG_m=GG_ma-GG_mn;
 	}
-	a=aa-an;
-cout<<"Usted tiene: " <<a<< "años" <<m<< "meses" "y" <<d<< "dias";
+	GG_a=GG_aa-GG_an;
+
+cout<<"//=======Nombre del programa: LA EDAD"<<endl;
+cout<<"//====Archivo: GironGenesis-laedad.cpp"<<endl;
+cout<<"//====Autor: Giron Genesis"<<endl;
+cout<<"//====Fecha de elaboración: 02-05-2022"<<endl;
+cout<<"//====Fecha de última actualización: 19-05-2022"<<endl;
+
+cout<<"Usted tiene: " <<GG_a<< "años" <<GG_m<< "meses" "y" <<GG_d<< "dias";
 	return(0);
 }

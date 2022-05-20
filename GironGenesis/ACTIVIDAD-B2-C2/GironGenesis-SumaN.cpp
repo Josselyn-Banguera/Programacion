bebf8856bcffc586a45e838ebab829cd1aad0983
Genesis Girón <genesis.giron.lupu@utelvt.edu.ec>
2022-05-20 00:12:32 -0500

    Actualización SumaN B2_C2

--- GironGenesis/ACTIVIDAD-B2-C2/GironGenesis-SumaN.cpp
+++ GironGenesis/ACTIVIDAD-B2-C2/GironGenesis-SumaN.cpp
@@ -1,19 +1,30 @@
+////=======Nombre del programa: SUMA DE DOS NÚMEROS
+//====Archivo: GironGenesis-SumaN.cpp
+//====Autor: Giron Genesis
+//====Fecha de elaboración: 02-05-2022                     //====Fecha de última actualización: 19-05-2022
+
 #include<iostream>
 using namespace std; 
 int main()
 {
-	int c= 0,n;
-	float s=0,x;
+	int GG_c= 0, GG_n;
+	float GG_s=0, GG_x;
 cout<<"Ingrese la cantidad de valor a suma : ";
-cin>>n;
+cin>>GG_n;
 
    do{ 
-	cout<<"Ingrese el elemento"<<c+1;
-	cin>>x;
-	c=c+1;
-	s=s+x;
- }while(c<n) ;
-cout<<"La suma de los elementos fue : "<<s<<endl;
+	cout<<"Ingrese el elemento: ";
+	cin>>GG_x;
+	GG_c=GG_c+1;
+	GG_s=GG_s+GG_x;
+ }while (GG_c<GG_n);
+   
+cout<<"//=======Nombre del programa: SUMA DE DOS NÚMEROS"<<endl;
+cout<<"//====Archivo: GironGenesis-SumaN.cpp"<<endl;
+cout<<"//====Autor: Giron Genesis"<<endl;
+cout<<"//====Fecha de elaboración: 02-05-2022"<<endl;
+cout<<"//====Fecha de última actualización: 19-05-2022"<<endl;
+
+     cout<<"La suma de los elementos fue : "<<GG_s<<endl;
 return(0);
 }
-

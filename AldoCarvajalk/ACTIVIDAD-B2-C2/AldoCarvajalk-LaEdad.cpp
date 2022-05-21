c82f376dfaaba0b16118b93b185ed2c559f89885
AldoCarvajalk <aldo.carvajal.klinger@utelvt.edu.ec>
2022-05-20 22:35:00 -0500

    Actualizacion de La edad B2-C2

--- AldoCarvajalk/ACTIVIDAD-B2-C2/AldoCarvajalk-LaEdad.cpp
+++ AldoCarvajalk/ACTIVIDAD-B2-C2/AldoCarvajalk-LaEdad.cpp
@@ -1,28 +1,41 @@
+//=======Nombre del programa: la edad
+//====Archivo: AldoCarvajalk-laEdad.cpp
+//====Autor: Carvajal Aldo
+//====Fecha de creacion: 03-05-2022
+//====Fecha de ultima actualizacion: 19-05-2022
+
 #include<iostream>
 using namespace std;
 
 int main ()
 {
-	int AA,MA,DA,AN,MN,DN,A,M,D;
-	cout<<"Ingrese la fecha actual (AAAA MM DD): ";
-	cin>>AA>>MA>>DA;
-	cout<<"Ingrese su fecha de nacimiento (AAAA MM DD): ";
-	cin>>AN>>MN>>DN;
-	if(DA>=DN){
-	D=DA-DN;
+	int CA_aa,CA_ma,CA_da,CA_an,CA_mn,CA_dn,CA_a,CA_m,CA_d;
+	cout<<"Ingrese la fecha actual (Dia Mes Año): ";
+	cin>>CA_da>>CA_ma>>CA_aa;
+	cout<<"Ingrese su fecha de nacimiento (Dia Mes Año):";
+	cin>>CA_dn>>CA_mn>>CA_an;
+	if(CA_da<CA_dn){
+	CA_da=CA_da+30;
+	CA_ma=CA_ma-1;
+	CA_da=CA_da-CA_dn;
 	}else{
-	DN=DN+30;
-	MA=MA-1;
-	D=DA-DN;
+	CA_d=CA_da-CA_dn;
 	}
-	if(MA>=MN){
-	M=MA-MN;
+	if(CA_ma<CA_mn){
+		CA_ma=CA_ma+12;
+		CA_aa=CA_aa-1;
+		CA_m=CA_ma-CA_mn;
 	}else{
-	MN=MN+12;
-	AA=AA-1;
-	M=MA-MN;
+	CA_m=CA_ma-CA_mn;
 	}
-	A=AA-AN;
-	cout<<"Usted tiene "<<A<<" años "<<M<<" meses  y"<<D<<" dias ";
+	CA_a=CA_aa-CA_an;
+
+cout<<"//=======Nombre del programa: la edad "<<endl;
+cout<<"//====Archivo: AldoCarvajalk-laEdad.cpp "<<endl;
+cout<<"//====Autor: Carvajal Aldo "<<endl;
+cout<<"//====Fecha de creacion: 03-05-2022 "<<endl;
+cout<<"//====Fecha de ultila actualizacion: 19-05-2022 "<<endl;
+
+	cout<<"Usted tiene "<<CA_a<<" años "<<CA_m<<" meses  y"<<CA_d<<" dias "<<endl;
 	return(0);
 }

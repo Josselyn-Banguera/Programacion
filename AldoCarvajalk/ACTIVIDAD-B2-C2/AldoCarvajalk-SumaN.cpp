bb51b949167ef6cdeb9899d23230d8ab10ba6188
AldoCarvajalk <aldo.carvajal.klinger@utelvt.edu.ec>
2022-05-21 01:06:07 -0500

    Actualizacion de Suma De Dos Numeros B2-C2

--- AldoCarvajalk/ACTIVIDAD-B2-C2/AldoCarvajalk-SumaN.cpp
+++ AldoCarvajalk/ACTIVIDAD-B2-C2/AldoCarvajalk-SumaN.cpp
@@ -1,19 +1,32 @@
+//=======Nombre del programa: Suma de dos numeros
+//====Archivo: AldoCarvajalk-SumaN.cpp
+//====Autor: Carvajal Aldo
+//====Fecha de elaboracion: 03-05-2022
+//====Fecha de ultima actualizacion: 19-05-2022
+
 #include<iostream>
 using namespace std;
 
 int main()
 {
-	int c=0, n;
-	float s=0, x;
+	int CA_c=0, CA_n;
+	float CA_s=0, CA_x;
 	cout<<"Ingrese la cantidad de numeros a sumar : ";
-	cin>>n;
+	cin>>CA_n;
 	do{
          
 		cout<<"Ingrese un numero :";
-		cin>>x;
-		c=c+1;
-		s=s+x;
-	}while(c<n);
-	cout<<"La suma de los numeros es :"<<s<<endl;
+		cin>>CA_x;
+		CA_c=CA_c+1;
+		CA_s=CA_s+CA_x;
+	}while(CA_c<CA_n);
+
+//=======Nombre del programa: Suma de dos numeros
+//====Archivo: AldoCarvajalk-SumaN.cpp
+//====Autor: Carvajal Aldo
+//====Fecha de elaboracion: 03-05-2022
+//====Fecha de ultima actualizacion: 19-05-2022
+
+	cout<<"La suma de los numeros es :"<<CA_s<<endl;
 	return(0);
 }

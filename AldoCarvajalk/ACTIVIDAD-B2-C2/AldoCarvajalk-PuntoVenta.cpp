1aae5acba1c890bc19337e5becbaa73e678d63a5
AldoCarvajalk <aldo.carvajal.klinger@utelvt.edu.ec>
2022-05-21 00:50:57 -0500

    Actualizacion de Punto venta B2-C2

--- AldoCarvajalk/ACTIVIDAD-B2-C2/AldoCarvajalk-PuntoVenta.cpp
+++ AldoCarvajalk/ACTIVIDAD-B2-C2/AldoCarvajalk-PuntoVenta.cpp
@@ -1,29 +1,42 @@
+//=======Nombre del programa: Punto de venta
+//====Nombre del archivo: AldoCarvajalk-Compara.cpp
+//====Autor: Carvajal Aldo
+//====Fecha de elaboracion: 03-05-2022
+//====Fecha de ultima actualizacion: 19-05-2022
+
 #include<iostream>
 using namespace std;
 int main()
 {
-	int c=0, P;
-	float A=0,x,Tb,Piva,Pdsc,iva=0.12,dsc=0.30,vt;
+	int CA_c=0, CA_P;
+	float CA_A=0,CA_x,CA_Tb,CA_Piva,CA_Pdsc,CA_iva=0.12,CA_dsc=0.30,CA_vt;
 	cout<<"Ingrese la cantidad de productos: ";
-	cin>>P;
+	cin>>CA_P;
 	do{
 		cout<<"Ingrese el valor de los productos: ";
-		cin>>x;
-		c=c+1;
-		A=A+x;
-		Piva=A*iva;
-		cout<<"El valor del iva de la compra es de: $"<<Piva<<endl;
-		Pdsc=A*dsc;
-		cout<<"El valor del descuento de su compra es de:$"<<Pdsc<<endl;
-		Tb=Piva+Pdsc;
-	}while(c<P);
-	if(Tb==A){
-		cout<<"Su compra aplica para un descuento: "<<Tb<<endl;
-	iva=Tb*Piva/100;
-	dsc=Tb*Pdsc/100;
+		cin>>CA_x;
+		CA_c=CA_c+1;
+		CA_A=CA_A+CA_x;
+		CA_Piva=CA_A*CA_iva;
+		cout<<"El valor del iva de la compra es de: $"<<CA_Piva<<endl;
+		CA_Pdsc=CA_A*CA_dsc;
+		cout<<"El valor del descuento de su compra es de:$"<<CA_Pdsc<<endl;
+		CA_Tb=CA_Piva+CA_Pdsc;
+	}while(CA_c<CA_P);
+	if(CA_Tb==CA_A){
+		cout<<"Su compra aplica para un descuento: "<<CA_Tb<<endl;
+	CA_iva=CA_Tb*CA_Piva/100;
+	CA_dsc=CA_Tb*CA_Pdsc/100;
 }else{
-	vt=A+Piva-Pdsc;
-	cout<<"El valor total es de:$ "<<vt<<endl;
+	CA_vt=CA_A+CA_Piva-CA_Pdsc;
+
+//=======Nombre del programa: Punto de venta
+//====Nombre del archivo: AldoCarvajalk-Compara.cpp
+//====Autor: Carvajal Aldo
+//====Fecha de elaboracion: 03-05-2022
+//====Fecha de ultima actualizacion: 19-05-2022
+
+	cout<<"El valor total es de:$ "<<CA_vt<<endl;
 }
 return(0);
 }

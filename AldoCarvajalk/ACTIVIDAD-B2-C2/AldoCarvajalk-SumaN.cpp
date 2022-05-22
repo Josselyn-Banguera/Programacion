8727fed7995e33a7405f8324e120b0f2dbbc2575
Aldocarvajalk <aldo.carvajalk.klinger@utelvt.edu.ec>
2022-05-22 02:23:04 -0500

    Actualizacion de Suma de varios numeros c.pp desde mi computador B2-C2

--- AldoCarvajalk/ACTIVIDAD-B2-C2/AldoCarvajalk-SumaN.cpp
+++ AldoCarvajalk/ACTIVIDAD-B2-C2/AldoCarvajalk-SumaN.cpp
@@ -21,11 +21,11 @@ int main()
 		CA_s=CA_s+CA_x;
 	}while(CA_c<CA_n);
 
-//=======Nombre del programa: Suma de dos numeros
-//====Archivo: AldoCarvajalk-SumaN.cpp
-//====Autor: Carvajal Aldo
-//====Fecha de elaboracion: 03-05-2022
-//====Fecha de ultima actualizacion: 19-05-2022
+cout<<"//=======Nombre del programa: Suma de dos numeros"<<endl;
+cout<<"//====Archivo: AldoCarvajalk-SumaN.cpp"<<endl;
+cout<<"//====Autor: Carvajal Aldo"<<endl;
+cout<<"//====Fecha de elaboracion: 03-05-2022"<<endl;
+cout<<"//====Fecha de ultima actializacion: 19-05-2022"<<endl;
 
 	cout<<"La suma de los numeros es :"<<CA_s<<endl;
 	return(0);
